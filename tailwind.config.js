8e9f85e893c747a67a0bb78fa16f59c1e04e4adf
renaldin <renaldinoviandi0@gmail.com>
2022-07-17 17:49:34 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    sm: '640px',
+
+    md: '768px',
+
+    lg: '1024px',
+
+    xl: '1280px',
+
+    '2xl': '1536px',
+  },
+  plugins: [],
+}
